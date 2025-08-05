04962ef945986d91824cfbd0aa0bb5f5f62bdcb6
Jo <jcgedeon86@gmail.com>
2025-08-05 11:05:48 -0400

    Add files via upload

---
+++ AYKs/AYK10/scripts/script.js
@@ -0,0 +1,13 @@
+/* 
+name: Jo Gedeon
+file name: script.js
+date: August 2 2025
+*/
+
+//Function to display content
+function content(){
+    var text = document.getElementById("new")
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
